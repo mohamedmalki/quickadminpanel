c72b1401411ad54629715de7ffce58ffc17020da
Mohamed MALKI <malki.mohamed.upm@gmail.com>
2019-09-13 09:11:01 +0100

    Update UsersTableSeeder.php

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,7 +12,7 @@ class UsersTableSeeder extends Seeder
                 'id'             => 1,
                 'name'           => 'Admin',
                 'email'          => 'admin@admin.com',
-                'password'       => '$2y$10$M50Wg8n72D8clAPbQIFdA.MHUIRSQl6opHrxsF0dk3KngWKHAHWvi',
+                'password'       => bcrypt('password'),//'$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
                 'remember_token' => null,
                 'created_at'     => '2019-09-12 08:39:23',
                 'updated_at'     => '2019-09-12 08:39:23',
